e2d4bf27c7ef89d15531f7e7b3532ee0de8a7aeb
kanorix <kanorix320@gmail.com>
2024-08-29 18:27:47 +0000

    bug: 時間の登録ができないバグの修正

--- frontend/api/requests/task/postTask.ts
+++ frontend/api/requests/task/postTask.ts
@@ -2,5 +2,8 @@ import client from "@/api/client";
 import { TaskInput } from "@/components/tasks/types";
 
 export const postTask = ({ task }: { task: TaskInput }): Promise<TaskInput> => {
-  return client.post("/api/tasks", task);
+  return client.post("/api/tasks", {
+    ...task,
+    scheduled_time: task.scheduledTime,
+  });
 };
